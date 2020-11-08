4eab75a3e7940afd1e61f3af9ad0a1e188bec39e
Andrew Bui <andrew.bui.au@gmail.com>
2020-11-08 17:03:32 +1100

    added full authentication and login functionality to application

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:restricted]
+
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'pages/restricted'
   devise_for :users
   root to: "pages#home"
+  get "/restricted", to: "pages#restricted", as: "restricted"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
