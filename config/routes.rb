9b9df60257d72ceb4d95e02582bad8a62f0fb32d
Andrew Bui <andrew.bui.au@gmail.com>
2020-11-08 16:00:23 +1100

    initialised devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/restricted'
+  devise_for :users
+  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
