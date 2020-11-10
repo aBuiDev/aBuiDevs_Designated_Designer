0879699ab2158d57a2fe2b2d7b09b3284f7806ee
Andrew Bui <andrew.bui.au@gmail.com>
2020-11-11 01:45:50 +1100

    formatted repository

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/restricted'
-  devise_for :users
-  root to: "pages#home"
-  get "/restricted", to: "pages#restricted", as: "restricted"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
