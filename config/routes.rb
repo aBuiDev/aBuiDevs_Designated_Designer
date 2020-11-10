61d7b3d33f95c4d35fab0147e22edb6edbd68620
Andrew Bui <andrew.bui.au@gmail.com>
2020-11-11 10:00:31 +1100

    pre-initialised application for devise implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Pages(main) Routes
+  root to: 'pages#index'
+
+  # Projects Routes
   get '/projects', to: 'projects#index'
   get '/projects/new', to: 'projects#new'
   post '/projects/new', to: 'projects#create'
