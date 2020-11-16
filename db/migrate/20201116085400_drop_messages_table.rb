9ff48633962e98ad17c4c0543f8b1f978ef9cbe6
Andrew Bui <andrew.bui.au@gmail.com>
2020-11-17 01:33:44 +1100

    further clearning of database migrations

--- db/migrate/20201116085400_drop_messages_table.rb
+++
@@ -1,9 +0,0 @@
-class DropMessagesTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :messages do |t|
-      t.bigint :project_id, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
---
+++ db/migrate/20201116143315_drop_messages_table.rb
@@ -0,0 +1,4 @@
+class DropMessagesTable < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
