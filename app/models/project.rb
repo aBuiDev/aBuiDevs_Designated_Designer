2593c17797cd0b062545294f12d677728bfa86a3
Andrew Bui <andrew.bui.au@gmail.com>
2020-11-19 16:07:02 +1100

    added feature to display inaccuration user input

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,14 @@
 class Project < ApplicationRecord
   belongs_to :client
   belongs_to :designer, optional: true
+  has_one_attached :picture
+  has_one :chatbox
+
   enum payment_status: [ "Pending Payment", "Payment Complete" ]
   enum designer_status: [ "Planning", "Awaiting Draft Approval", "Design Complete" ]
   enum client_status: [ "Reviewing Draft", "Reviewing Final Design", "Design Approved" ]
   enum project_status: [ "Draft", "With Designer", "Pending Approval", "Project Complete" ]
-  has_one_attached :picture
-  has_one :chatbox
+
+  validates :title, :description, :picture, presence: true
 end
+
